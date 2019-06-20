eb1675daacb5526113bd50b6e51c0e9460c638f9
Wesley Smith <me@wesjd.net>
2019-06-20 17:12:08 -0400

    rebind events when kicked

--- src/bot.js
+++ src/bot.js
@@ -8,53 +8,57 @@ const debug = new Debugger("bot:main")
 let bot = createBot(config.mineflayer)
 
 initialize().catch(err => debug("Couldn't start brain", err))
+bindEvents()
 
-bot.on("login", () => {
-    bot.chat("/server mixed")
-})
+function bindEvents() {
+    bot.on("login", () => {
+        bot.chat("/server mixed")
+    })
 
-bot.on("end", message => {
-    debug("Kicked/ended for", message)
-    bot = createBot(config.mineflayer)
-})
+    bot.on("end", message => {
+        debug("Kicked/ended for", message)
+        bot = createBot(config.mineflayer)
+        bindEvents()
+    })
 
-bot.on("message", chatMessage => {
-    const message = chatMessage.toString()
-    const matches = /(\(Team\)\ |\[PM\] From )?(\[.+\]\ )?<?\*?([a-zA-Z0-9_-]{3,15})>?:\ (.+)/.exec(message)
-    if (matches && matches.length > 1) {
-        const data = {
-            isTeamChat: matches[1] == "(Team) ",
-            isPM: matches[1] == "[PM] From ",
-            staffRank: matches[2],
-            username: matches[3],
-            message: matches[4]
-        }
-        if (data.isPM || data.message.toLowerCase().startsWith(bot.username.toLowerCase())) {
-            data.message = data.message.substring(bot.username.length).trim()
-
-            let prefix = "/g"
-            if (data.isTeamChat) {
-                prefix = ""
-            } else if (data.isPM) {
-                prefix = "/msg " + data.username + " "
+    bot.on("message", chatMessage => {
+        const message = chatMessage.toString()
+        const matches = /(\(Team\)\ |\[PM\] From )?(\[.+\]\ )?<?\*?([a-zA-Z0-9_-]{3,15})>?:\ (.+)/.exec(message)
+        if (matches && matches.length > 1) {
+            const data = {
+                isTeamChat: matches[1] == "(Team) ",
+                isPM: matches[1] == "[PM] From ",
+                staffRank: matches[2],
+                username: matches[3],
+                message: matches[4]
             }
+            if (data.isPM || data.message.toLowerCase().startsWith(bot.username.toLowerCase())) {
+                data.message = data.message.substring(bot.username.length).trim()
 
-            const splits = data.message.split(" ")
-            const command = matchers.player.command[splits[0]]
-            if (command) {
-                data.message = splits.slice(1).join(" ")
-                const response = command(data, bot)
-                if (response != undefined && response != null) {
-                    bot.chat(prefix + response)
+                let prefix = "/g"
+                if (data.isTeamChat) {
+                    prefix = ""
+                } else if (data.isPM) {
+                    prefix = "/msg " + data.username + " "
                 }
-            } else {
-                runMatchers(data, matchers.player.other, prefix).catch(err => debug("Couldn't run matchers", err))
+
+                const splits = data.message.split(" ")
+                const command = matchers.player.command[splits[0]]
+                if (command) {
+                    data.message = splits.slice(1).join(" ")
+                    const response = command(data, bot)
+                    if (response != undefined && response != null) {
+                        bot.chat(prefix + response)
+                    }
+                } else {
+                    runMatchers(data, matchers.player.other, prefix).catch(err => debug("Couldn't run matchers", err))
+                }
             }
+        } else {
+            runMatchers(message, matchers.server, "/g ").catch(err => debug("Couldn't run matchers", err))
         }
-    } else {
-        runMatchers(message, matchers.server, "/g ").catch(err => debug("Couldn't run matchers", err))
-    }
-})
+    })
+}
 
 async function runMatchers(args, array, prefix = "") {
     let response
